9887bfb1165ff67d8a3e4ec5e87368619655bc82
V. Hemanth Kumar <hemanthvadapalli5@gmail.com>
2025-09-22 16:12:51 +0530

    Add files via upload

---
+++ equornot.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+	int a,b;
+	scanf("%d",&a);   //first integer
+	scanf("%d",&b);   //second integer
+	if(a==b)
+		printf("The numbers are equal\n");
+	else
+		printf("Not equal");
+	return 0;
+}
